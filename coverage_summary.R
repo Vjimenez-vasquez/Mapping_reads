4757025903e5aa7e4b5b4597d31944d260f20279
Victor Jimenez-Vasquez <89874227+Vjimenez-vasquez@users.noreply.github.com>
2023-03-28 11:19:21 -0500

    Add files via upload

---
+++ coverage_summary.R
@@ -0,0 +1,21 @@
+library(tidyr) ;
+d <- paste0(getwd(),"/","tsv")
+setwd(d)
+r <- dir() ;
+s <- gsub("_S.*","",r) ;
+a <- 0 ;
+b <- 0 ;
+b1 <- 0 ;
+b2 <- 0 ;
+for (i in 1:length(s)){ 
+  a <- read.csv(r[i], header=T) 
+  b <- s[i] 
+  b1 <- append(b1,b)
+  b2 <- append(b2,a)
+}
+b3 <- data.frame(sample=b1,data=as.character(b2))
+b4 <- separate(b3,"data",into=c("reference","startpos","endpos","numreads","covbases","coverage","meandepth","meanbaseq","meanmapq"),sep="\t") 
+b5 <- b4[2:length(b4$reference),]
+b5
+write.table(b5,"coverages.tsv", sep="\t", row.names=F, quot=F)
+q("no") ;
